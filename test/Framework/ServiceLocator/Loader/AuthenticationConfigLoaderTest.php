9e27b8d84914a726068784cc1ecdd3b80ddb4586
Vincent van Dijk <info@vincentvandijk.com>
2017-12-06 14:33:02 +0100

    Added NotAuthenticatedMiddleware.

--- test/Framework/ServiceLocator/Loader/AuthenticationConfigLoaderTest.php
+++ test/Framework/ServiceLocator/Loader/AuthenticationConfigLoaderTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace ExtendsFramework\Authentication\Framework\ServiceLocator\Loader;
 
 use ExtendsFramework\Authentication\AuthenticatorInterface;
+use ExtendsFramework\Authentication\Framework\Http\Middleware\NotAuthenticatedMiddleware;
 use ExtendsFramework\Authentication\Framework\ServiceLocator\Factory\AuthenticatorFactory;
+use ExtendsFramework\Http\Middleware\Chain\MiddlewareChainInterface;
 use ExtendsFramework\ServiceLocator\Resolver\Factory\FactoryResolver;
 use ExtendsFramework\ServiceLocator\ServiceLocatorInterface;
 use PHPUnit\Framework\TestCase;
@@ -27,6 +29,9 @@ class AuthenticationConfigLoaderTest extends TestCase
                 FactoryResolver::class => [
                     AuthenticatorInterface::class => AuthenticatorFactory::class,
                 ],
+            ],
+            MiddlewareChainInterface::class => [
+                NotAuthenticatedMiddleware::class => 130,
             ],
             AuthenticatorInterface::class => [
                 'realms' => [],
