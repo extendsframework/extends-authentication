5045780b60700dc579e927ba5f44f990998c2da6
Vincent van Dijk <info@vincentvandijk.com>
2017-12-27 20:10:11 +0100

    Reformatted code.

--- src/Framework/Http/Middleware/NotAuthenticatedMiddleware.php
+++ src/Framework/Http/Middleware/NotAuthenticatedMiddleware.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace ExtendsFramework\Authentication\Framework\Http\Middleware;
 
+use ExtendsFramework\Authentication\AuthenticationException;
 use ExtendsFramework\Http\Middleware\Chain\MiddlewareChainInterface;
 use ExtendsFramework\Http\Middleware\MiddlewareInterface;
 use ExtendsFramework\Http\Request\RequestInterface;
@@ -10,7 +11,6 @@ use ExtendsFramework\Http\Response\Response;
 use ExtendsFramework\Http\Response\ResponseInterface;
 use ExtendsFramework\Logger\LoggerInterface;
 use ExtendsFramework\Logger\Priority\Notice\NoticePriority;
-use ExtendsFramework\Authentication\AuthenticationException;
 
 class NotAuthenticatedMiddleware implements MiddlewareInterface
 {
